5b00b20f0483419eafe6b31e67dba63050af101d
AlphaniaMuthee <walphania2@gmail.com>
2020-04-19 23:55:59 +0300

    -

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -3,3 +3,9 @@ import java.util.*;
 public class Ci {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("What plain statement do you want to have encrypted...");
+        String plain = sc.nextLine();
+        System.out.println(" Specify your the value of change you'd ike on your text... ");
+        int change = sc.nextInt();
+        String encrypted = "";
+        char alphabet;
