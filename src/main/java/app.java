d6294503cae3423e39948151aa328ca1fd55f1d9
AlphaniaMuthee <walphania2@gmail.com>
2020-04-19 23:56:28 +0300

    add code

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -9,3 +9,33 @@ public class Ci {
         int change = sc.nextInt();
         String encrypted = "";
         char alphabet;
+        for(int i=0; i < plain.length();i++)
+        {
+            alphabet = plain.charAt(i);
+
+            if (alphabet>= 'a' && alphabet<= 'z')
+            {
+                alphabet = (char) (alphabet + change);
+
+                if(alphabet> 'z') {
+                    alphabet = (char) (alphabet+'a'-'z'-1);
+                }
+                encrypted = encrypted + alphabet;
+            }
+
+            else if (alphabet>= 'A' && alphabet<= 'Z')
+            {
+                alphabet = (char) (alphabet + change);
+
+                if(alphabet> 'Z') {
+                    alphabet = (char) (alphabet+'A'-'Z'-1);
+                }
+                encrypted = encrypted + alphabet;
+            }
+            else  {
+                encrypted = encrypted + alphabet;
+            }
+        }
+        System.out.println("This is your encrypted text: " + encrypted);
+    }
+}
